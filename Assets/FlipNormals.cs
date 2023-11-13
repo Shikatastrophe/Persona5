d7250634d0df58ae1d44130d6b9f237d83ffa725
Shikatastrophe <shikatastrophe@gmail.com>
2023-11-13 08:56:22 -0600

    asddasdasdasdasdas

---
+++ Assets/FlipNormals.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FlipNormals : MonoBehaviour
+{
+    void Awake()
+    {
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        Mesh mesh = skinnedMeshRenderer.sharedMesh;
+        Vector3[] normals = mesh.normals;
+        for (int i = 0; i < normals.Length; i++)
+        {
+            normals[i] = -normals[i];
+        }
+        mesh.normals = normals;
+
+        int[] triangles = mesh.triangles;
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            int t = triangles[i];
+            triangles[i] = triangles[i + 2];
+            triangles[i + 2] = t;
+        }
+        mesh.triangles = triangles;
+
+        skinnedMeshRenderer.sharedMesh = mesh;
+    }
+}
+
